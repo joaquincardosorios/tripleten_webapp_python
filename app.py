32d8af2c76b0587aea367868e7b0740b8a3d0d92
Joaquin Cardoso <joaquin.cardoso.r@gmail.com>
2024-02-06 18:09:04 -0300

    slider_button

--- app.py
+++ app.py
@@ -2,4 +2,10 @@ import streamlit as st
 
 st.header('Lanzar una moneda')
 
-st.write('Es aplicacion no es funcional. En construccion')
+number_of_trials = st.slider('¿Npumero de intentos?',1,1000,10)
+start_button = st.button('Ejecutar')
+
+if start_button:
+    st.write(f'Experimento con {number_of_trials} intentos en curso.')
+
+st.write('Aun no funciona')
